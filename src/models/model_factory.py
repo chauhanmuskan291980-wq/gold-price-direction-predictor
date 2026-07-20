3e1e09dadca2077c548a11fd79280187b57df317
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-07-20 23:53:51 +0530

    feat(models): add factory for multiple classifiers

---
+++ src/models/model_factory.py
@@ -0,0 +1,50 @@
+from sklearn.ensemble import(
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def build_model()->dict:
+    """
+    create all machine-learning models used by the project.
+
+    Returns:
+    Dictionary containing model names and model objects.
+    """
+    return {
+        "logistic_regression":Pipeline(
+            steps=[
+                ("scaler",StandardScaler()),
+                (
+                    "classifier",
+                    LogisticRegression(
+                        max_iter =1000,
+                        class_weight="balanced",
+                        random_state=42,
+                    )
+                )
+            ]
+        ),
+
+        "random_forest":RandomForestClassifier(
+         n_estimators=300,
+         max_depth=5,
+         min_samples_leaf=10,
+         class_weight="balanced",
+         random_state=42,
+         n_jobs=1
+        ),
+
+        "gradient_boosting":GradientBoostingClassifier(
+            n_estimators=150,
+            learning_rate=0.03,
+            max_depth=2,
+            min_samples_leaf=15,
+            random_state=42,
+        )
+
+    }
